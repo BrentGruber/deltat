71b62545148ab0678d5a97d96a480e6669d20e6b
Brent Gruber <brent.gruber77@gmail.com>
2021-06-03 00:37:32 -0400

    adding basic otel instrumentation, TODO: set exporter to tempo

--- src/CoreService/coreservice/main.py
+++ src/CoreService/coreservice/main.py
@@ -2,6 +2,18 @@ import time
 import uvicorn
 from fastapi import FastAPI, Request
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+)
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+
 from router import router as v1_router
 from config import settings
 
@@ -19,6 +31,8 @@ async def add_process_time_header(request: Request, call_next):
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
+
+FastAPIInstrumentor.instrument_app(app)
 
 if __name__ == "__main__":
     if settings.ENVIRONMENT.upper() == "LOCAL":
